6bc58b5f50dc788654ce12e876afbfc01d777aee
Matias Castillo <mati@tsf-500-wpa-1-200.epfl.ch>
2025-05-11 12:13:33 +0200

    Added v1.1 of arduinos codes

---
+++ arduinos/master/master.ino
@@ -0,0 +1,125 @@
+#include "Wire.h"
+#include <Adafruit_PWMServoDriver.h>
+
+// NOTE : Consider adding sleep mode when servo_change is false
+
+#define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  625 // this is the 'maximum' pulse length count (out of 4096) 
+
+const int length_data_Servos = 4;
+const int length_data_slave1 = 3;
+const int length_data_slave2 = 3;
+const int nbr_data_expected = length_data_Servos + length_data_slave1 + length_data_slave2;
+
+const byte adress_slave1 = 0x01;
+const byte adress_slave2 = 0x02;
+Adafruit_PWMServoDriver board = Adafruit_PWMServoDriver(0x40);
+
+
+int data_Servos[length_data_Servos] = {0};
+byte data_slave1[length_data_slave1];
+byte data_slave2[length_data_slave2];
+bool transmission_allowed = 0;
+
+// Création des fonctions de transmission de données : 
+
+bool servo_change = false;
+void sendServosData();
+int angleToPulse(int ang);
+
+void read_data()
+{
+  for (int i = 0; i<nbr_data_expected; ++i)
+    {
+      if (i < length_data_slave1)
+      {
+        data_slave1[i] = Serial.read();
+        continue;
+      }
+
+      if (i >= length_data_slave1 && i < length_data_slave2 + length_data_slave1)
+      {
+        data_slave2[i-length_data_slave1] = Serial.read();
+        continue;
+      }
+
+      if (i >= length_data_slave2 + length_data_slave1)
+      {
+        int new_val(Serial.read());
+        int j(i-length_data_slave2 - length_data_slave1);
+
+        if (new_val != data_Servos[j])
+        {
+          data_Servos[j] = new_val;
+          servo_change = true;
+        }
+
+      }      
+    }
+  
+  transmission_allowed = 1;
+
+}
+
+
+void sendToNano(byte address, byte data[], byte length) {
+  Wire.beginTransmission(address);
+  Wire.write(data, length);
+  Serial.print("Sending to Nano value ");
+  for (unsigned i(0); i < 3; i++)
+  {
+    Serial.print((int8_t)(*(data+i)));
+    Serial.print(" ");
+  }
+  Serial.println();
+  Wire.endTransmission();
+}
+
+
+// Fonctions principales du programme : 
+
+
+void setup() 
+{
+  Serial.begin(9600);                                                                                                                                                                                                                                                         
+  Wire.begin();
+}
+
+void loop()
+{
+  transmission_allowed = 0;
+
+  if (Serial.available() >= nbr_data_expected)
+  {
+    read_data();
+
+    if (transmission_allowed)
+    {
+      sendToNano(adress_slave1, data_slave1, length_data_slave1);
+      sendToNano(adress_slave2, data_slave2, length_data_slave2);
+      if (servo_change)
+      {
+        sendServosData();
+      }
+      transmission_allowed = 0;
+    }
+  }
+  delay(1);
+}
+
+void sendServosData()
+{
+  // for (unsigned i = 0; i < length_data_Servos; i++)
+  // {
+  //   board.setPWM(i, 0, angleToPulse(data_Servos[i]));
+  // }
+}
+
+int angleToPulse(int ang) {                            //gets the angle in degree and returns the pulse width  
+  int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);  // map angle of 0 to 180 to Servo min and Servo max 
+  // Serial.print("Angle: ");
+  // Serial.print(ang);
+  // Serial.print(" pulse: ");
+  // Serial.println(pulse);
+  return pulse;
+}
---
+++ arduinos/slave/slave.ino
@@ -0,0 +1,144 @@
+#include "Wire.h"
+
+enum SERVO_SIDE {LEFT = 0x01, RIGHT = 0x02};
+const byte adress_slave1 = LEFT;           // Utiliser 0x02 pour le moteur 2 et changer pinouts, sinon même code
+
+
+const int FrontIN1 =  (adress_slave1 == LEFT) ? 3   : 11;
+const int FrontIN2 =  (adress_slave1 == LEFT) ? 5   : 10;                     // Moteurs avant et arrière (même côté)
+const int CentreIN1 = (adress_slave1 == LEFT) ? 6   : 9;
+const int CentreIN2 = (adress_slave1 == LEFT) ? 9   : 6;
+const int BackIN1 =   (adress_slave1 == LEFT) ? 10  : 5;
+const int BackIN2 =   (adress_slave1 == LEFT) ? 11  : 3;
+
+const int length_data = 3;
+bool data_received = false;
+int8_t received_data[length_data];
+
+int speed1 = 0;                           // Valeur de 0 à 255 pour la vitesse des moteur
+bool direction1 = 0;                        // 0 pour l'avant et 1 pour l'arrière
+
+int speed2 = 0;                           // Valeur de 0 à 255 pour la vitesse des moteur
+bool direction2 = 0;                        // 0 pour l'avant et 1 pour l'arrière
+
+// Inutile pour l'instant mais sera utile si on utilise 1 driver par moteur
+int speed3 = 0;                           // Valeur de 0 à 255 pour la vitesse des moteur
+bool direction3 = 0;                        // 0 pour l'avant et 1 pour l'arrière
+
+void ClearBuffer()
+{
+  while (Wire.available())
+  {
+    Wire.read();
+  }
+}
+
+void receiveEvent(int numBytes)
+{
+  if (numBytes >= length_data)
+  {
+    for (unsigned i = 0; i < length_data; i++) {
+      received_data[i] = (int8_t)Wire.read();
+    }
+    // ClearBuffer(); //safety net
+    data_received = true;
+  }
+}
+
+void AttributeSpeedsDir()
+{
+  speed1 = abs(received_data[0]);
+  direction1 = received_data[0] < 0;
+  if (speed1 == 128)
+  {
+    speed1--;
+  }
+  speed1 *= 2;    // Avoir une plage de 0 à 254
+
+  speed2 = abs(received_data[1]);
+  direction2 = received_data[1] < 0;
+  if (speed2 == 128)
+  {
+    speed2--;
+  }
+  speed2 *= 2;    // Avoir une plage de 0 à 254
+
+  speed3 = abs(received_data[2]);
+  direction3 = received_data[2] < 0;
+  if (speed3 == 128)
+  {
+    speed3--;
+  }
+  speed3 *= 2;    // Avoir une plage de 0 à 254
+}
+
+void TransferSpeedsDir()
+{
+  speed1, speed2, speed3 = abs(speed1), abs(speed2), abs(speed3);
+  if (speed1 <= 6)
+  {
+    digitalWrite(FrontIN1, LOW);
+    digitalWrite(FrontIN2, LOW); 
+  } 
+  else if (direction1 == 0)
+  {
+    analogWrite(FrontIN1, speed1);
+    digitalWrite(FrontIN2, LOW);
+  } else {
+    digitalWrite(FrontIN1, LOW);
+    analogWrite(FrontIN2, speed1);
+  }
+
+  if (speed2 <= 6)
+  {
+    digitalWrite(CentreIN1, LOW);
+    digitalWrite(CentreIN2, LOW); 
+  } 
+  else if (direction2 == 0)
+  {
+    analogWrite(CentreIN1, speed2);
+    digitalWrite(CentreIN2, LOW);
+  } else {
+    digitalWrite(CentreIN1, LOW);
+    analogWrite(CentreIN2, speed2);
+  }
+
+  if (speed3 <= 6)
+  {
+    digitalWrite(BackIN1, LOW);
+    digitalWrite(BackIN2, LOW); 
+  } 
+  else if (direction3 == 0)
+  {
+    analogWrite(BackIN1, speed3);
+    digitalWrite(BackIN2, LOW);
+  } else {
+    digitalWrite(BackIN1, LOW);
+    analogWrite(BackIN2, speed3);
+  }
+}
+
+
+
+void setup() 
+{
+  Wire.begin(adress_slave1);  
+  Wire.onReceive(receiveEvent);
+  pinMode(FrontIN1, OUTPUT);
+  pinMode(FrontIN2, OUTPUT);
+  pinMode(CentreIN1, OUTPUT);
+  pinMode(CentreIN2, OUTPUT);
+  pinMode(BackIN1, OUTPUT);
+  pinMode(BackIN2, OUTPUT);
+}
+
+void loop() 
+{
+  if (data_received)
+  {
+    AttributeSpeedsDir();
+    TransferSpeedsDir();
+  }
+  data_received = false;
+}
+

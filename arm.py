359de77992b8208e1dea7a8ba861279df54bbc89
Li-keRaptor <matthias.anastaze@epfl.ch>
2025-05-13 00:23:10 +0200

    Correction 2/3 bugs + debuggage léger
    Il faudra juste ne pas oublier de supprimer les lignes inutiles de print. Elles peuvent tjrs servir entre temps.

--- arm.py
+++ arm.py
@@ -8,7 +8,7 @@ TETA_MAX = 90
 TETA_MIN = -90
 PHI_MIN = -90
 PHI_MAX = 90
-SENSIBILITY = 0.1
+SENSIBILITY = 0.5
 
 # Valeurs du programme
 teta = 45
@@ -19,19 +19,24 @@ coude : bool = 0    # Si 0 on a coude haut // 1 pour un coude bas
 
 # Code principal :
 def set_init_pos():     # Au moment de démarrer le robot
+    global coude, teta, phi, Px, Py
+    coude = 0
     teta = 45           # Positions connues stables
-    phi = 45
-    Px = L1 * np.cos(teta * np.pi / 180 ) + L2 * np.cos(teta * np.pi / 180  - phi * np.pi / 180 )
-    Py = L1 * np.sin(teta * np.pi / 180 ) + L2 * np.sin(teta * np.pi / 180  - phi* np.pi / 180 )
+    phi = -45
+    Px = L1 * np.cos(teta * np.pi / 180 ) + L2 * np.cos(teta * np.pi / 180  + phi * np.pi / 180 )
+    Py = L1 * np.sin(teta * np.pi / 180 ) + L2 * np.sin(teta * np.pi / 180  + phi* np.pi / 180 )
+    print("Init teta = ", teta, " Init phi = ", phi, "Init Px = ", Px, "Init Py = ", Py)
     
     
 
 def inverse_kinematics(new_Px, new_Py):
     # Distance au point cible
+    global coude, teta, phi, Px, Py
     dist = np.hypot(new_Px, new_Py)
 
     # Vérifier si le point est atteignable
     if dist > (L1 + L2) or dist < abs(L1 - L2):
+        print("Erreur 1 : Distance = ", dist, " trop élevée")
         return 1                                       # Erreur détectée
 
     # Calcul de D
@@ -46,14 +51,32 @@ def inverse_kinematics(new_Px, new_Py):
     new_teta = np.arctan2(new_Py, new_Px) - np.arctan2(L2 * np.sin(new_phi), L1 + L2 * np.cos(new_phi))
 
     # Vérifications d'angles
-    if (new_teta < TETA_MIN or new_teta > TETA_MAX or new_phi < PHI_MIN or new_phi > PHI_MAX):
-        return 1                                        # Erreur détectée
+    if (new_teta < TETA_MIN * np.pi / 180 or new_teta > TETA_MAX * np.pi / 180 or
+            new_phi < PHI_MIN * np.pi / 180 or new_phi > PHI_MAX * np.pi / 180):
+        
+        # On teste avec un coude inversé si nécessaire :
+        coude = not coude
+        print("Appel du changement de coude")
+        
+        if coude :
+            new_phi = np.arccos(D)      # Solution coude bas
+        else :                      
+            new_phi = -np.arccos(D)     # Solution coude haut
+
+        new_teta = np.arctan2(new_Py, new_Px) - np.arctan2(L2 * np.sin(new_phi), L1 + L2 * np.cos(new_phi))
+        
+        if (new_teta < TETA_MIN * np.pi / 180 or new_teta > TETA_MAX * np.pi / 180 or
+            new_phi < PHI_MIN * np.pi / 180 or new_phi > PHI_MAX * np.pi / 180):
+            coude = not coude
+            print("Erreur 2 : Position non atteignable avec les angles donnés")
+            return 1                                        # Erreur détectée
     
     # Ecriture des nouvelles valeurs de position + conversion en degrés
     Px = new_Px
     Py = new_Py
     teta = new_teta * 180 / np.pi
     phi = new_phi * 180 / np.pi
+    print("New teta = ", teta, " New phi = ", phi, "New Px = ", Px, "New Py = ", Py)
     
     return 0                                              # Pas d'erreur détectée
 
@@ -66,7 +89,15 @@ def arm_modification(x_joystick, y_joystick):
         print("Erreur détectée sur le calcul")
     
     
-    
+# Test de valeurs :
+
+set_init_pos()
+for i in range (0, 50):
+    arm_modification(-0.4, 0.4)
+print("deuxième partie")
+for i in range (0, 50):
+    arm_modification(-0.4, -0.4)
+
     
     
     
